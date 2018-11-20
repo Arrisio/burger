5e3aabbdd4b1187a093e5bacad4b7692537ad383
User <zemlak@list.ru>
2018-11-20 19:27:29 +0300

    a

--- app/js/module.js
+++ app/js/module.js
@@ -1,1 +1,21 @@
-console.log('Привет мир');
+const menu_btn = document.querySelector('#menu_btn'),
+	ham_menu = document.querySelector('#ham_menu'),
+	ham_list = document.querySelector('#ham_list'),
+	body = document.querySelector('body');
+
+function toggleMenu() {
+	menu_btn.classList.toggle('is-active');
+	ham_menu.classList.toggle('is-active');
+	body.classList.toggle('is-locked');
+};
+
+menu_btn.addEventListener('click', function (e) {
+	e.preventDefault();
+	toggleMenu();
+});
+ham_list.addEventListener('click', function (e) {
+	let el = e.target;
+	if (el.tagName == 'A') {
+		toggleMenu();
+	};
+});

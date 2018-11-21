3d4079d7475767e4aff0908ebfad666813f4a087
User <zemlak@list.ru>
2018-11-21 20:09:40 +0300

    create accord js

--- app/js/module.js
+++ app/js/module.js
@@ -5,7 +5,7 @@ const menuBtn = document.querySelector('.hamburger-menu-link'),
 hamListItems = document.querySelectorAll('.nav_hamburger .nav__item');
 
 
-function sleep(ms) {
+function аsleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 async function toggleMenu() {
@@ -35,3 +35,36 @@ document.addEventListener("keydown", function (e) {
 		toggleMenu();
 	}
 });
+
+
+allAccordions = document.querySelectorAll('.accordion');
+
+function findParentAccordion(elt) {
+	let i = 0; // защита от зациклавания
+	while (!(elt.classList.contains('accordion')) && i < 100) {
+		elt = elt.parentNode;
+	}
+	return elt;
+};
+
+for (let iAccord = 0; iAccord < allAccordions.length; iAccord++) {
+	accordMenu = allAccordions[iAccord].getElementsByTagName("A");
+	for (let iItem = 0; iItem < accordMenu.length; iItem++) {
+
+		accordMenu[iItem].addEventListener('click', function (e) {
+			e.preventDefault();
+			clickedAccordMenuItems = findParentAccordion(this).getElementsByTagName("LI");
+			parentLi = this.parentNode;
+			for (let i = 0; i < clickedAccordMenuItems.length; i++) {
+				if (parentLi == clickedAccordMenuItems[i]) {
+					clickedAccordMenuItems[i].classList.toggle('is-active');
+					console.log('toggle', clickedAccordMenuItems[i]);
+				} else {
+					clickedAccordMenuItems[i].classList.remove('is-active');
+					console.log('rm', clickedAccordMenuItems[i]);
+				}
+			}
+		});
+
+	}
+};

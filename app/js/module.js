90b145bd3e5b7db6d3dd0edbd883b6c0685fe5be
User <zemlak@list.ru>
2018-11-22 19:41:33 +0300

    refactor accordion

--- app/js/module.js
+++ app/js/module.js
@@ -37,34 +37,34 @@ document.addEventListener("keydown", function (e) {
 });
 
 
-allAccordions = document.querySelectorAll('.accordion');
+allAccordions = document.getElementsByClassName('accordion');
+
+
 
 function findParentAccordion(elt) {
 	let i = 0; // защита от зациклавания
-	while (!(elt.classList.contains('accordion')) && i < 100) {
-		elt = elt.parentNode;
+	while (
+		!(elt.parentElement.classList.contains('accordion')) &&
+		i < 100
+	) {
+		elt = elt.parentElement;
 	}
 	return elt;
-};
+}
 
-for (let iAccord = 0; iAccord < allAccordions.length; iAccord++) {
-	accordMenu = allAccordions[iAccord].getElementsByTagName("A");
-	for (let iItem = 0; iItem < accordMenu.length; iItem++) {
 
-		accordMenu[iItem].addEventListener('click', function (e) {
-			e.preventDefault();
-			clickedAccordMenuItems = findParentAccordion(this).getElementsByTagName("LI");
-			parentLi = this.parentNode;
-			for (let i = 0; i < clickedAccordMenuItems.length; i++) {
-				if (parentLi == clickedAccordMenuItems[i]) {
-					clickedAccordMenuItems[i].classList.toggle('is-active');
-					console.log('toggle', clickedAccordMenuItems[i]);
-				} else {
-					clickedAccordMenuItems[i].classList.remove('is-active');
-					console.log('rm', clickedAccordMenuItems[i]);
-				}
+for (let accordList of allAccordions) {
+	accordList.addEventListener('click', function (e) {
+		e.preventDefault();
+		clickedAccordItem = findParentAccordion(e.target);
+		clickedAccordMenuItems = clickedAccordItem.parentElement.getElementsByTagName("LI");
+
+		for (let accordItem of clickedAccordMenuItems) {
+			if (accordItem == clickedAccordItem) {
+				accordItem.classList.toggle('is-active');
+			} else {
+				accordItem.classList.remove('is-active');
 			}
-		});
-
-	}
+		}
+	})
 };
